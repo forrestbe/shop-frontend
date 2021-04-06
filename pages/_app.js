1afbf00adbdfbe80e5a8d5160ef32a60a5b4bb62
forrestbe <f.brownlie@keylight.de>
2021-04-06 21:19:41 +0200

    remove uneeded component

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,3 @@
-import '@styles/globals.css'
-
 function Application({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,4 @@
 import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
 
 export default function Home() {
   return (
