601862490c667664196f399a85dadff23bb47cdf
forrestbe <f.brownlie@keylight.de>
2021-04-07 09:10:58 +0200

    add production endpoint

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
 export const endpoint = `http://localhost:3000/api/graphql`;
-export const prodEndpoint = `http://localhost:3000/api/graphql`;
+export const prodEndpoint = `https://shop-backend-zzllt.ondigitalocean.app/api/graphql`;
 export const perPage = 2;
